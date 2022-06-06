f53392233d6f8a39cc59e287873ea070b4dec395
Shannon Reid <shannon.reid@snyk.io>
2022-06-06 13:40:33 -0400

    adding vulns

--- mix.exs
+++ mix.exs
@@ -15,6 +15,15 @@ defmodule Testproj.Mixfile do
   defp deps do
     [
       {:mix_erlang_tasks, "0.1.0"},
+      {:hackney, "~> 1.15"},
+      {:ecto, "~> 0.11.2"},
+      {:postgrex, "~> 0.8.1"},
+      {:cowboy, "~> 2.9"},
+      {:tzdata, "~> 1.1"},
+      {:jason, "~> 1.3"},
+      {:plug, "~> 1.0"},
+      {:xain, "~> 0.6.2"},
+      {:sweet_xml, "~> 0.6.6"},
     ]
   end
 end
